c4dcca15d9481bf306dba9a104ec0d4f63b63d77
Nick <>
2021-01-14 22:39:09 +0300

    Homework done

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int initsum = 2_000_000_000;
+        int transfer = 500_000_000;
+        int sumaftertranser;
+
+        sumaftertranser = initsum + transfer;
+        System.out.println(sumaftertranser);
+	// write your code here
+    }
+}
